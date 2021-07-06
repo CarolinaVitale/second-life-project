5d9b5a5766a63f0164269fb4a26273e27bfac572
CarolinaVitale <vitale.carola@gmail.com>
2021-07-06 17:43:31 +0200

    changes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
+const AddressAPI = require('../services/api-handler')
+const api = new AddressAPI()
+
 
 
 const User = require("../models/User.model")
+const { count } = require('../models/User.model')
 
 
 // Signup
@@ -10,23 +14,40 @@ router.get('/signup', (req, res) => res.render('auth/signup'))
 
 
 router.post('/signup', (req, res) => {
-    const { username, firstName, lastName, profileImg, address, lat, lng, pwd, email, phoneNumber } = req.body
     
- 
-    const location = {
-        type: 'Point',
-        coordinates: [lat, lng]
-    }
+    // API address
+    const streetSearch = req.body.street
+    let lat, lng
 
-    const bcryptSalt = 10
-    const salt = bcrypt.genSaltSync(bcryptSalt)
-    const hashPass = bcrypt.hashSync(pwd, salt)
+     api.getCoordinates(streetSearch)
+        .then(response => {
+            lat = response.data.results[0].geometry.location.lat
+            lng = response.data.results[0].geometry.location.lng
+        })
+        .then(() => {        
+                    const { username, firstName, lastName, profileImg, pwd, email, phoneNumber, street, zipCode, city, country } = req.body
+                
+                    const location = {
+                        type: 'Point',
+                        coordinates: [lat, lng]
+                    }
+                
+                
+                    const address = { street, zipCode, city, country, location }
+                    console.log('COORDENADAS', lat, lng)
+                
+                    
+                    const bcryptSalt = 10
+                    const salt = bcrypt.genSaltSync(bcryptSalt)
+                    const hashPass = bcrypt.hashSync(pwd, salt)
+                
+                    User
+                        .create({ username, firstName, lastName, profileImg, address, pwd: hashPass, email, phoneNumber })
+                        .then(() => res.send(req.body)) // redirect profile page
+                        .catch(err => console.log(err))
+        })
 
-    User
-        .create({ username, firstName, lastName, profileImg, address, location, pwd: hashPass, email, phoneNumber })
-        .then(() => res.send(req.body)) // redirect profile page
-        .catch(err => console.log(err))
-})
+    })
 
 //Login
 router.get('/login', (req, res) => res.render('auth/login'))
