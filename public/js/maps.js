20d4b0ef12350ce87c23c81c5946cdc9379c5312
CarolinaVitale <vitale.carola@gmail.com>
2021-07-07 09:47:09 +0200

    maps

--- public/js/maps.js
+++ public/js/maps.js
@@ -2,7 +2,7 @@ function initMap() {
 
     const myMap = new google.maps.Map(
         document.querySelector('#userAddress'),
-        { zoom: 14, center: { lat, lng } }
+        { zoom: 14, center: { lat: 40.4415188, lng:-3.671316 } }
     )
 
     getPlacesData(myMap)
@@ -19,12 +19,12 @@ function getPlacesData(myMap) {
 
 
 
-function printMarker(location, myMap) {
+// function printMarker(location, myMap) {
 
-    const { coordinates } = location
+//     const { coordinates } = location
 
-    new google.maps.Marker({
-        map: myMap,
-        position: { lat: coordinates[0], lng: coordinates[1] }
-    })
-}
+//     new google.maps.Marker({
+//         map: myMap,
+//         position: { lat: coordinates[0], lng: coordinates[1] }
+//     })
+// }
--- routes/api.routes.js
+++ routes/api.routes.js
@@ -7,7 +7,6 @@ router.get('/users', (req, res) => {
     User
         .find()
         .then(users => res.json(users))
-        console.log('ESTOS SON LOS USUARIOS', users)
         .catch(err => console.log(err))
 })
 
